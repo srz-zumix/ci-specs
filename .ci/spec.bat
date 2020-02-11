2c8ac04607488e1d7491526ae3f75f778a09c9c4
srz_zumix <zumix.cpp@gmail.com>
2020-02-11 21:55:41 +0900

    test

--- .ci/spec.bat
+++ .ci/spec.bat
@@ -1,7 +1,9 @@
 @echo off
 
+echo %cd%
+
 if exist "%ProgramFiles%\Git\bin\bash.exe" (
-    "%ProgramFiles%\Git\bin\bash.exe" -c "./spec.sh"
+    "%ProgramFiles%\Git\bin\bash.exe" -c "./.ci/spec.sh"
 ) else (
     echo bash not found..
 )
