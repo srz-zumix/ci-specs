f278fb94a4da9372966d4784e98ba6948b6e8371
srz_zumix <zumix.cpp@gmail.com>
2020-04-22 21:41:08 +0900

    space

--- .ci/vswhere.bat
+++ .ci/vswhere.bat
@@ -4,24 +4,24 @@
 SET LIST=false
 
 if defined VS90COMNTOOLS (
-    SET LIST=!LIST!,2009
+    SET LIST=!LIST!, 2009
 )
 if defined VS100COMNTOOLS (
-    SET LIST=!LIST!,2010
+    SET LIST=!LIST!, 2010
 )
 if defined VS110COMNTOOLS (
-    SET LIST=!LIST!,2012
+    SET LIST=!LIST!, 2012
 )
 if defined VS120COMNTOOLS (
-    SET LIST=!LIST!,2013
+    SET LIST=!LIST!, 2013
 )
 if defined VS140COMNTOOLS (
-    SET LIST=!LIST!,2015
+    SET LIST=!LIST!, 2015
 )
 
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
     for /f "usebackq" %%v in ( `"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property catalog_productLineVersion` ) do (
-        SET LIST=!LIST!,%%v
+        SET LIST=!LIST!, %%v
     )
 )
 
