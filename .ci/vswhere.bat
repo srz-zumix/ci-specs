916545e28a79959e9c0d988bbdf5f9cbf38fa498
srz_zumix <zumix.cpp@gmail.com>
2020-04-22 11:39:03 +0900

    add vs2015 or older

--- .ci/vswhere.bat
+++ .ci/vswhere.bat
@@ -1,12 +1,28 @@
 @echo off
 
-if not exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
-    echo false
-) else (
-    @setlocal enabledelayedexpansion
-    SET LIST=false
+@setlocal enabledelayedexpansion
+SET LIST=false
+
+if defined VS90COMNTOOLS (
+    SET LIST=!LIST!,2009
+)
+if defined VS100COMNTOOLS (
+    SET LIST=!LIST!,2010
+)
+if defined VS110COMNTOOLS (
+    SET LIST=!LIST!,2012
+)
+if defined VS120COMNTOOLS (
+    SET LIST=!LIST!,2013
+)
+if defined VS140COMNTOOLS (
+    SET LIST=!LIST!,2015
+)
+
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
     for /f "usebackq" %%v in ( `"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property catalog_productLineVersion` ) do (
         SET LIST=!LIST!,%%v
     )
-    echo !LIST:false,=!
 )
+
+echo !LIST:false,=!
