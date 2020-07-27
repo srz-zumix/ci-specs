cf67a09c878d6931b1189887aba173b02a72e28e
srz_zumix <zumix.cpp@gmail.com>
2020-07-27 23:02:45 +0900

    update spec

--- .ci/spec.sh
+++ .ci/spec.sh
@@ -182,8 +182,14 @@ echo ------------------------
 
 # DATE=$(TZ="Asia/Tokyo" date)
 DATE=$(date -u)
-echo $CI_ENV_NAME
-echo $CI_ENV_GIT_COMMIT
+echo name    : $CI_ENV_NAME
+echo commit  : $CI_ENV_GIT_COMMIT
+echo branch  : $CI_ENV_GIT_BRANCH
+echo base    : $CI_ENV_GIT_BASE_BRANCH
+echo source  : $CI_ENV_GIT_SOURCE_BRANCH
+echo target  : $CI_ENV_GIT_TARGET_BRANCH
+echo tag     : $CI_ENV_GIT_TAG
+echo tag name: $CI_ENV_GIT_TAG
 
 echo "NPROC : ${NUMBER_OF_PROCESSORS}"
 echo "ARCH  : ${ARCH}"
