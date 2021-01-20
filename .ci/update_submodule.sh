f6b14c1afe770db89e291ce813c69005687e2d9d
srz_zumix <zumix.cpp@gmail.com>
2021-01-20 11:22:17 +0900

    config

--- .ci/update_submodule.sh
+++ .ci/update_submodule.sh
@@ -5,6 +5,9 @@ git submodule init
 git submodule update
 git submodule foreach git pull origin master
 
+git config --global user.name "srz-zumix"
+git config --global user.email "srz-zumix@users.noreply.github.com"
+
 git add -A
 git commit -m "update head [ci skip]" || true
 # git push https://${GITHUB_TOKEN}@github.com/srz-zumix/ci-spec.git master
